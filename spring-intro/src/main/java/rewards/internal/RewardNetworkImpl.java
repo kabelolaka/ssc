609dbdea8be35c1ed0ce467bf6950dd6d1b32a13
kabelo <Kabelo.Laka@momentum.co.za>
2017-08-25 17:13:35 +0200

    Modified code for reward an account for dinning and corresponding test

--- spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -1,5 +1,7 @@
 package rewards.internal;
 
+import common.money.MonetaryAmount;
+import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
@@ -41,9 +43,11 @@ public class RewardNetworkImpl implements RewardNetwork {
 	public RewardConfirmation rewardAccountFor(Dining dining) {
         final Account account = accountRepository.findByCreditCard(dining.getCreditCardNumber());
         final Restaurant merchantNumber = restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
-
-        // TODO-01: Reward an account per the sequence diagram
-		// TODO-02: Return the corresponding reward confirmation
-		return null;
+		//Reward an account
+		MonetaryAmount monetaryAmount = merchantNumber.calculateBenefitFor(account, dining);
+		AccountContribution accountContribution = account.makeContribution(monetaryAmount);
+		//Return the corresponding reward confirmation
+		RewardConfirmation rewardConfirmation = rewardRepository.confirmReward(accountContribution, dining);
+		return rewardConfirmation;
 	}
 }
